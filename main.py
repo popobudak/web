87417525c33351de75096ecde10c3a27903775fc
popobudak <popobudak@gmail.com>
2026-05-29 07:53:55 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -583,6 +583,6 @@ with app.app_context():
     print("✅ Database siap - Semua tabel dibuat")
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 4440))
     print(f"🚀 License Manager berjalan di port {port}")
     app.run(host='0.0.0.0', port=port, debug=False)
